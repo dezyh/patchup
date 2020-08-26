d2f9e912aedd39b48749d5d92854f1f6f9813cc6
Ben Mitchell <dezyh@outlook.com>
2020-08-26 19:00:35 +1000

    fix missing test page

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -5,7 +5,6 @@ import Update from '../pages/update';
 import Login from '../pages/login';
 import SignUp from '../pages/signup';
 import Dashboard from '../pages/dashboard';
-import Test from '../pages/test';
 
 const Main = () => {
   return (
@@ -16,7 +15,6 @@ const Main = () => {
             <Route exact path='/signup' component={SignUp} />
             <Route exact path='/dashboard' component={Dashboard} />
             <Route exact path='/update' component={Update} />
-            <Route exact path='/test' component={Test} />
         </Switch>
     </React.Fragment>
   );
