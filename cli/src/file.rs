da50b3b69052ecd2b8acabbb0e588236a028f686
Ben Mitchell <dezyh@outlook.com>
2021-04-11 16:54:28 +1000

    Add basic directory plan for diffing
    Currently only supports mutual files, additions and deletions

---
+++ cli/src/file.rs
@@ -0,0 +1,43 @@
+use walkdir::WalkDir;
+use std::path::PathBuf;
+use std::hash::{Hash, Hasher};
+use std::collections::HashSet;
+
+pub struct FileInfo {
+    pub base: String,
+    pub path: String,
+} 
+
+impl FileInfo {
+
+    // Parses a single file
+    pub fn from_file(base: &str, file: &PathBuf) -> FileInfo {
+        FileInfo {
+            base: base.to_string(),
+            path: file.strip_prefix(base).unwrap().to_str().unwrap().to_string(),
+        }
+    }
+    
+    // Parses all files in a directory
+    pub fn from_dir(dir: &str) -> HashSet<FileInfo> {
+        let files: HashSet<FileInfo> = WalkDir::new(&dir)
+            .into_iter()
+            .map(|entry| entry.unwrap().into_path())
+            .filter(|path| path.is_file())
+            .map(|file| FileInfo::from_file(&dir, &file))
+            .collect();
+        files
+    }
+}
+
+impl Hash for FileInfo {
+    fn hash<H: Hasher>(&self, _state: &mut H) {}
+}
+
+impl PartialEq for FileInfo {
+    fn eq(&self, other: &Self) -> bool {
+        self.path == other.path
+    }
+}
+
+impl Eq for FileInfo {}
