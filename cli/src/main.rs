93187ae92c85353907f743462947863da0eadadd
Ben Mitchell <dezyh@outlook.com>
2021-04-13 21:19:22 +1000

    Create command line interface

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,21 +2,10 @@ mod diff;
 mod file;
 mod patch;
 mod plan;
+mod cli;
 
-use patch::Patch;
-use plan::Plan;
+use cli::Cli;
 
 fn main() {
-    let source = String::from("/home/dezyh/coding/patchup/cli/test/old");
-    let target = String::from("/home/dezyh/coding/patchup/cli/test/new");
-    let output = String::from("/home/dezyh/coding/patchup/cli/test/patch.up");
-
-    let plan = Plan::new(source.clone(), target.clone());
-    plan.print();
-
-    let patch = Patch::from_plan(plan);
-    patch.write_file(&output);
-
-    let patch = Patch::from_file(output);
-    patch.apply(&source);
+    Cli::new()
 }
