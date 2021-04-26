d26e5baff72b0ca25c6ac951dcc341a86b57832d
Ben Mitchell <dezyh@outlook.com>
2021-04-26 22:46:56 +1000

    Make signin page a form

--- client/pages/signin.tsx
+++ client/pages/signin.tsx
@@ -2,24 +2,59 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { SlideFade, Flex, Heading, Input, Button, useColorModeValue } from '@chakra-ui/react'
 import { signInRequest } from '../store/user/actions'
+import { Formik, Form, Field } from 'formik'
+import { FormControl, FormErrorMessage } from '@chakra-ui/react'
 
 const SignIn = () => {
   const formBackground = useColorModeValue('gray.200', 'gray.700')
   const dispatch = useDispatch()
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const submit = (e) => {
-    e.preventDefault()
-    dispatch(signInRequest(username, password))
-  }
+
   return (
     <SlideFade in offsetY='20px'>
       <Flex height='100vh' alignItems='center' justifyContent='center'>
         <Flex direction='column' background={formBackground} p={12} rounded={6}>
           <Heading mb={6}>Sign In</Heading>
-          <Input onChange={e => setUsername(e.target.value)} placeholder='username' variant='filled' mb={3} type='username' />
-          <Input onChange={e => setPassword(e.target.value)} placeholder='password' variant='filled' mb={6} type='password' />
-          <Button colorScheme='blue' onClick={(e) => submit(e)}>Sign In</Button>
+
+            <Formik
+              initialValues={{
+                username: '',
+                password: '',
+                email: '',
+                firstname: '',
+                lastname: '',
+              }}
+              onSubmit={(values, actions) => {
+                const { username, password } = values
+                dispatch(signInRequest(username, password))
+              }}
+            >
+              <Form>
+
+                <Field name='username'>
+                  {({ field, form }) => (
+                    <FormControl isInvalid={form.errors.username && form.touched.username}>
+                      <Input {...field} id='username' placeholder='Username' type='username' mb={3} variant='filled'/>
+                      <FormErrorMessage>{form.errors.username}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+
+
+                <Field name='password'>
+                  {({ field, form }) => (
+                    <FormControl isInvalid={form.errors.password && form.touched.password}>
+                      <Input {...field} id='password' placeholder='Password' type='password' mb={3} variant='filled'/>
+                      <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+              
+                <Button mt={3} colorScheme='blue' type='submit'>Sign In</Button>
+
+              </Form>
+            
+            </Formik>
+
         </Flex>
       </Flex>
     </SlideFade>
